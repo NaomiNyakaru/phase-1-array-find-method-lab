cce6dea57d35118cd87bf3c1f0f79d9aa0af78b8
NaomiNyakaru <naomi.nyakaru@gmail.com>
2024-07-03 14:11:13 +0300

    find methods lab

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // code your solution here
+function superbowlWin(record){
+    return record.find(element => element.result === "W").year;
+}
+
+console.log(superbowlWin(record));
+
+function superbowlWin(record) {
+    const win = record.find(element => element.result === "W");
+    return win ? win.year : undefined;
+  }
